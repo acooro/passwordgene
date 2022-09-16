b272610cedc8b6a38c0d33cf719ccb8f9b3c2d8b
Dong Nguyen <dnguye621@gmail.com>
2022-09-16 09:51:56 -0500

    update buttom

--- script.js
+++ script.js
@@ -12,3 +12,30 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+// Global variables
+var numOfCharacters;
+var useLowerCaseLtrs;
+
+// ask the user how many characters should be in the password
+function howManyCharacters(){
+  numOfCharacters = parseInt( prompt("How many characters?") );
+}
+
+function useLowerCase(){
+  useLowerCaseLtrs = confirm("Do you want lower case letters in the password?");
+  console.log(useLowerCaseLtrs)
+}
+
+
+function generatePassword() {
+  console.log("start here");
+  howManyCharacters();
+  useLowerCase();
+
+
+
+  return "finalpassword"
+}
